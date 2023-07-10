7f3eb8e8ffd3415e1756f67821ae72b9a421b620
Anil Vadlamudi <anil.vadlamudi@hartekdss.com>
2023-07-10 14:49:08 +0530

    moved to src folder

---
+++ src/data.js
@@ -0,0 +1,77 @@
+{
+    "users": [
+      {
+        "product_name": "Living Room Set",
+        "price": "99999",
+        "id": 1
+      },
+      {
+        "product_name": "Bedroom Corner Set",
+        "price": "99999",
+        "id": 2
+      },
+
+      {
+        "product_name": "Clo",
+        "price": "HartekDSS",
+        "id": 3
+      },
+
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 4
+      },
+
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 5
+      },
+
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 6
+      },
+
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 7
+      },
+
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 8
+      },
+
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 9
+      },
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 10
+
+      },
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 11
+      },
+      {
+        "product_name": "true",
+        "price": "HartekDSS",
+        "id": 12
+      }
+
+
+
+
+
+    ]
+}
